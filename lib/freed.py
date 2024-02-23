e018d66ef10182e2962eeea6006972eeed4834c7
cc123hh <1042369128@qq.com>
2024-02-23 13:09:41 +0800

    first commit

---
+++ lib/freed.py
@@ -0,0 +1,125 @@
+#stores raw bytes for encoding. Use the FreeDWrapper as an interface for regular ints
+class _FreeD:
+    identifier: 'bytes'
+    cameraID: 'bytes'
+    pitch: 'bytes'
+    yaw: 'bytes'
+    roll: 'bytes'
+    pos_z: 'bytes'
+    pos_y: 'bytes'
+    pos_x: 'bytes'
+    zoom: 'bytes'
+    focus: 'bytes'
+    reserved: 'bytes'
+
+    def __init__(self, pitch: 'int', yaw: 'int', roll: 'int', pos_z: 'int',  pos_y: 'int', pos_x: 'int', zoom: 'int', focus: 'int') -> None:
+        self.identifier = b'\xD1'
+        self.cameraID = b'\xFF'
+        self.reserved = b'\x00\x00'
+        self.pitch = pitch.to_bytes(3, 'big', signed=True)
+        self.yaw = yaw.to_bytes(3, 'big', signed=True)
+        self.roll = roll.to_bytes(3, 'big', signed=True)
+        self.pos_z = pos_z.to_bytes(3, 'big', signed=True)
+        self.pos_y = pos_y.to_bytes(3, 'big', signed=True)
+        self.pos_x = pos_x.to_bytes(3, 'big', signed=True)
+        self.zoom = zoom.to_bytes(3, 'big', signed=False)
+        self.focus = focus.to_bytes(3, 'big')
+
+    #calculates a checksum according to the protocol
+    @staticmethod
+    def checksum(*args) -> 'bytes':
+        checksum = int(0x40)
+        for arg in args:
+            for byte in arg:
+                checksum = (checksum - int(byte)) % 256
+
+        assert (checksum < 256 and checksum >= 0)
+        return checksum.to_bytes(1, 'big', False)
+
+    #encodes self into an array of bytes
+    def encode(self) -> 'bytes':
+        data = (self.identifier, self.cameraID, self.pitch, self.yaw,
+                self.roll, self.pos_z, self.pos_y, self.pos_x, self.zoom,
+                self.focus, self.reserved)
+
+        serial = self.identifier + self.cameraID + self.pitch + self.yaw + \
+            self.roll + self.pos_z + self.pos_y + self.pos_x + self.zoom + \
+            self.focus + self.reserved + _FreeD.checksum(*data)
+        assert (len(serial) == 29)  # somethings gone wrong if not
+        return serial
+
+
+# int type that is limited to 3 bytes (sort of a hack)
+class ThreeBytes(int):
+    field: 'int'
+    INT_MAX: 'int' = 0xFFFFFF
+    INT_MIN: 'int' = 0x00
+
+    def __init__(self, new: 'int'):
+        if (new > self.INT_MAX or new < self.INT_MIN):
+            raise ValueError("Maximum of 3 bytes allowed!")
+
+        self.field = new
+
+    def __add__(self, __x: int) -> int:
+
+        if (self.field + __x > self.INT_MAX or self.field + __x < self.INT_MIN):
+            raise OverflowError("Resulting value would be too large to store!")
+        return self.field + __x
+
+    def __sub__(self, __x: int) -> int:
+        if (self.field - __x > self.INT_MAX or self.field - __x < self.INT_MIN):
+            raise OverflowError("Resulting value would be too large to store!")
+        return self.field - __x
+
+    def __mul__(self, __x: int) -> int:
+        if (self.field * __x > self.INT_MAX or self.field * __x < self.INT_MIN):
+            raise OverflowError("Resulting value too large to store!")
+        return self.field * __x
+    
+    def to_bytes(self, length, byteorder:str, *, signed: bool = ...) -> bytes:
+        if byteorder not in ["little", "big"]:
+            raise Exception("arg_val must in : little,big")
+        return self.field.to_bytes(length, byteorder, signed)
+
+#signed variant of 3bytetype
+class ThreeBytesSigned(ThreeBytes):
+    INT_MAX: 'int' = 8388608
+    INT_MIN: 'int' = -8388608
+
+# native types for FreeD raw bytes
+class FreeDWrapper:
+    cameraid: int
+    pitch: 'ThreeBytesSigned'
+    yaw: 'ThreeBytesSigned'
+    roll: 'ThreeBytesSigned'
+    pos_z: 'ThreeBytesSigned'
+    pos_x: 'ThreeBytesSigned'
+    pos_y: 'ThreeBytesSigned'
+    zoom: 'ThreeBytes'
+    focus: 'ThreeBytes'
+
+    def __init__(self, cameraid: int, pitch: 'int', yaw: 'int', roll: 'int', pos_z: 'int',  pos_y: 'int', pos_x: 'int', zoom: 'int', focus: 'int') -> None:
+        if cameraid > 255 or cameraid < 0:
+            raise ValueError("CameraID must be between 0 and 255")
+        
+        self.cameraid = cameraid
+        self.pitch = ThreeBytesSigned(pitch)
+        self.yaw = ThreeBytesSigned(yaw)
+        self.roll = ThreeBytesSigned(roll)
+        self.pos_z = ThreeBytesSigned(pos_z)
+        self.pos_y = ThreeBytesSigned(pos_y)
+        self.pos_x = ThreeBytesSigned(pos_x)
+        self.zoom = ThreeBytes(zoom)
+        self.focus = ThreeBytes(focus)
+
+    #returns a struct with byte representations of fields, ready to encode into an array.
+    def createFreeD(self) -> '_FreeD':
+        struct = _FreeD(self.pitch, self.yaw, self.roll, self.pos_z, self.pos_y, self.pos_x, self.zoom, self.focus)
+        struct.cameraID = self.cameraid.to_bytes(1, 'big', False) 
+
+        return struct
+    
+    def encode(self) -> 'bytes':
+        message = self.createFreeD()
+        return message.encode()
---
+++ main.py
@@ -0,0 +1,83 @@
+import network,time,_thread
+from lib.microdot import Microdot,send_file,redirect
+
+wifi_ssid = "CJF-2.4G"
+wifi_pwd  = "cjfszy2002"
+
+def wifi_connect():
+    global wifi_ssid,wifi_pwd
+    if not wifi_ssid:
+        print("[Wlan]未配置wifi")
+        return False
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('[Wlan]开始尝试连接Wlan...')
+        wlan.connect(wifi_ssid, wifi_pwd)
+        times = 1
+        while not wlan.isconnected():
+            print(f"[Wlan][{times}]wlan try to connect...")
+            times += 1
+            time.sleep(0.5)
+            if times > 10:
+                print("[Wlan]密码ssid错误")
+                wifi_ssid = ""
+                wifi_pwd  = ""
+                wlan.active(False)
+                del wlan
+                return False
+    print(f'[Wlan]Connect success!\tIP:{wlan.ifconfig()[0]}', )
+    wlan.active(False)
+    del wlan
+    return True
+
+def wifi_scan():
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    return wlan.scan()
+
+def create_AP():
+    ap = network.WLAN(network.AP_IF)
+    ap.config(authmode=0,essid="esp32",password="")
+    ap.active(True)
+    return ap
+
+def start_configare_mode():
+    print("[wifi_configare] 开启配网模式")
+    ap = create_AP()
+    
+    app = Microdot()
+
+    @app.route('/')
+    async def index(request):
+        return send_file("/public/index.html")
+
+    @app.route('/scan')
+    async def scan(request):
+        wifiList = []
+        for wifi in wifi_scan():
+            wifiList.append(wifi[0].decode())
+        return wifiList
+
+    @app.route('/configareWifi',methods=["POST"])
+    async def configWifi(request):
+        global wifi_ssid,wifi_pwd
+        print(request.form)
+        wifi_ssid = request.form['ssid']
+        wifi_pwd = request.form['pwd']
+        if wifi_connect():
+            return redirect("/shutdown")
+        return "WiFi名称或者密码错误！"
+    
+    @app.route('/shutdown')
+    async def shutdown(req):
+        _thread.start_new_thread(lambda:(time.sleep(1),req.app.shutdown()),())
+        return "WiFi连接成功！服务即将关闭..."
+        
+    app.run(port=80,debug=True)
+
+def init():
+    if not wifi_connect():
+        start_configare_mode()
+
+# init()
